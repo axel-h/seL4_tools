47d539fcc5a9965be4aadf63ef3b2a1c8cea3864
Axel Heider <axelheider@gmx.de>
2023-05-08 01:31:01 +0200

    add support for VisionFive

--- cmake-tool/helpers/application_settings.cmake
+++ cmake-tool/helpers/application_settings.cmake
@@ -12,7 +12,7 @@ include_guard(GLOBAL)
 function(ApplyData61ElfLoaderSettings kernel_platform kernel_sel4_arch)
     set(
         binary_list
-        "tx1;hikey;odroidc2;odroidc4;imx8mq-evk;imx8mm-evk;hifive;tqma8xqp1gb;bcm2711;rocketchip"
+        "tx1;hikey;odroidc2;odroidc4;imx8mq-evk;imx8mm-evk;hifive;tqma8xqp1gb;bcm2711;rocketchip;visionfive"
     )
     set(efi_list "tk1;rockpro64;quartz64")
     set(uimage_list "tx2;am335x")
@@ -58,6 +58,9 @@ function(ApplyData61ElfLoaderSettings kernel_platform kernel_sel4_arch)
             set(ElfloaderArmV8LeaveAarch64 ON CACHE BOOL "" FORCE)
         endif()
         set(IMAGE_START_ADDR 0x10000000 CACHE INTERNAL "" FORCE)
+    endif()
+    if(KernelPlatformVisionFive)
+        set(IMAGE_START_ADDR 0x80400000 CACHE INTERNAL "" FORCE)
     endif()
 endfunction()
 
