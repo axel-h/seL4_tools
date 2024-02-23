8acb17c7a63d6264844aa3a8d64bf6399434271e
Axel Heider <axelheider@gmx.de>
2024-02-23 15:45:16 +0100

    elfloader: support different SBI implementations
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- elfloader-tool/src/arch-riscv/console.c
+++ elfloader-tool/src/arch-riscv/console.c
@@ -1,14 +1,18 @@
 /*
  * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ * Copyright 2021, HENSOLDT Cyber
  *
  * SPDX-License-Identifier: GPL-2.0-only
  */
 
-#include <elfloader_common.h>
+#include <autoconf.h>
+#include <elfloader/gen_config.h>
 #include "sbi.h"
 
+#if !defined(RISCV_SBI_NONE)
 int plat_console_putchar(unsigned int c)
 {
     sbi_console_putchar(c);
     return 0;
 }
+#endif
