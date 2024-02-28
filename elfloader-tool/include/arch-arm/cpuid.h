c11531c503131ab7c144c259c3894799f2f7576d
Axel Heider <axelheider@gmx.de>
2024-02-28 21:54:21 +0100

    elfloader: inline funcitons
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- elfloader-tool/include/arch-arm/cpuid.h
+++ elfloader-tool/include/arch-arm/cpuid.h
@@ -7,15 +7,10 @@
 #pragma once
 
 #include <types.h>
+#include <armv/machine.h>
 
-/* read ID register from CPUID */
-uint32_t read_cpuid_id(void);
-
-/* read MP ID register from CPUID */
-word_t read_cpuid_mpidr(void);
-
-/* check if CPU is in HYP/EL2 mode */
-word_t is_hyp_mode(void);
+#define MPIDR_MASK      0xff00ffffff
+#define CURRENTEL_EL2   (2 << 2)
 
 /* Pretty print CPUID information */
 void print_cpuid(void);
@@ -23,3 +18,27 @@ void print_cpuid(void);
 /* Returns the Cortex-Ax part number, or -1 */
 int get_cortex_a_part(void);
 
+
+/* read ID register from CPUID */
+static inline uint32_t read_cpuid_id(void)
+{
+    uint32_t val;
+    asm volatile("mrs %x0, midr_el1" : "=r"(val) :: "cc");
+    return val;
+}
+
+/* read MP ID register from CPUID , we only care about the affinity bits */
+static inline word_t read_cpuid_mpidr(void)
+{
+    uint64_t val;
+    asm volatile("mrs %x0, mpidr_el1" : "=r"(val) :: "cc");
+    return val & MPIDR_MASK;
+}
+
+/* check if CPU is in HYP/EL2 mode */
+word_t is_hyp_mode(void)
+{
+    uint32_t val;
+    asm volatile("mrs %x0, CurrentEL" : "=r"(val) :: "cc");
+    return (val == CURRENTEL_EL2);
+}
