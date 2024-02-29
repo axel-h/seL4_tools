474ea7190d79c7f7b19e32b437d4547784b22c01
Axel Heider <axelheider@gmx.de>
2024-02-29 02:48:21 +0100

    elfloader: make check evaluation more intuitive
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- elfloader-tool/src/arch-arm/sys_boot.c
+++ elfloader-tool/src/arch-arm/sys_boot.c
@@ -103,7 +103,7 @@ void main(UNUSED void *arg)
     memset(ctx, 0, sizeof(*ctx));
 
     /* initialize platform to a state where we can print to a UART */
-    if (initialise_devices()) {
+    if (0 != initialise_devices()) {
         printf("ERROR: Did not successfully return from initialise_devices()\n");
         abort();
     }
@@ -182,7 +182,7 @@ void continue_boot(int was_relocated)
      * driver model so all its pointers are set up properly.
      */
     if (was_relocated) {
-        if (initialise_devices()) {
+        if (0 != initialise_devices()) {
             printf("ERROR: Did not successfully return from initialise_devices()\n");
             abort();
         }
