dd240dbd387e33aebc59688e0b45622ca10da37b
Axel Heider <axelheider@gmx.de>
2024-02-29 19:02:58 +0100

    elfloader: enable more compiler warnings
    Also add comments about parameter dependencies.
    
    Signed-off-by: Axel Heider <axelheider@gmx.de>

--- elfloader-tool/include/printf.h
+++ elfloader-tool/include/printf.h
@@ -6,8 +6,9 @@
 
 #pragma once
 
+#define PRINTF_FUNC(pos_str, post_args) __attribute__((format(printf, pos_str, post_args)))
 #define NULL ((void *)0)
 #define FILE void
 
-int printf(const char *format, ...);
-int sprintf(char *buff, const char *format, ...);
+int printf(const char *format, ...) PRINTF_FUNC(1,2);
+int sprintf(char *buff, const char *format, ...) PRINTF_FUNC(2,3);
